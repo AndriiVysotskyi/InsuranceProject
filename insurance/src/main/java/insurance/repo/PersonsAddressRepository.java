b787845363fce0ef7c2cf22705d3caf2ad48ca18
Дарья <@activate.adobe.com>
2018-08-31 12:05:49 +0300

    adding

--- insurance/src/main/java/insurance/repo/PersonsAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package insurance.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import insurance.entities.AddressJpa;
-
-public interface PersonsAddressRepository extends JpaRepository<AddressJpa, Integer> {
-
-}
